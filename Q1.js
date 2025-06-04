53a93592e424bc70a8924752c8094b813f29bc92
Sayyed Sana <sayyedaamina12@gmail.com>
2025-06-04 11:39:26 +0530

    Initial commit for Assignment2

---
+++ Q1.js
@@ -0,0 +1,29 @@
+function reverseAndCombineWords(input) {
+  let words = input.trim().split(/\s+/);
+
+  while (words.length > 1) {
+    // Reverse each word
+    words = words.map(word => [...word].reverse().join(''));
+
+    // Combine pairs
+    words = words.reduce((acc, word, i, arr) => {
+      if (i % 2 === 0) {
+        // If it's the last element and no pair, just add it
+        const next = arr[i + 1] !== undefined ? arr[i + 1] : '';
+        acc.push(word + next);
+      }
+      return acc;
+    }, []);
+  }
+
+  return words[0];
+}
+
+console.log(reverseAndCombineWords("abc def")); 
+// Output: "cbafed"
+
+console.log(reverseAndCombineWords("abc def ghi 123")); 
+// Output: "defabc123ghi"
+
+console.log(reverseAndCombineWords("abc def gh34 434ff 55_eri 123 343")); 
+// Output: "43hgff434cbafed343ire_55321"
